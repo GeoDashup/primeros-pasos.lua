64f5b1f2f105528250f6e395f17b178cc11fdd38
GeoDashup <juanismael06@gmail.com>
2015-08-12 22:28:33 +0200

    Proyecto inicial
    Proyecto inicial

--- main.lua
+++ main.lua
@@ -3,4 +3,16 @@
 -- main.lua
 --
 -----------------------------------------------------------------------------------------
+local centroX = display.contentCenterX
+local centroY = display.contentCenterY
+
+local linea = display.newLine( centroX, centroY, 180 ,180 )
+linea:setStrokeColor(0, 0, 255)
+linea.strokeWidth = 15
+
+linea:append( 0, 0  )
+linea:append(320, 320)
+
+
+
 
