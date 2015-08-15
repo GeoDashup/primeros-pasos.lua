0f2a293545a30dd5e7104c52dc6ed2d569a52954
GeoDashup <juanismael06@gmail.com>
2015-08-15 18:12:10 +0200

    Cuadrado con color aleatorio
    Cuadrado con color aleatorio

--- main.lua
+++ main.lua
@@ -6,12 +6,26 @@
 local centroX = display.contentCenterX
 local centroY = display.contentCenterY
 
-local linea = display.newLine( centroX, centroY, 180 ,180 )
-linea:setStrokeColor(0, 0, 255)
-linea.strokeWidth = 15
 
-linea:append( 0, 0  )
-linea:append(320, 320)
+local valorRotacion = 0
+for i=1, 20 do
+
+local rectangulo = display.newRect( centroX, centroY, 	200, 200 )
+
+--local valorColor = math.random(255)
+
+rectangulo:setFillColor (math.random(120)/255, math.random(60)/255, math.random(200)/255 )
+ 
+
+
+valorRotacion = valorRotacion + 10
+
+rectangulo.rotation = valorRotacion
+
+end
+
+
+
 
 
 
